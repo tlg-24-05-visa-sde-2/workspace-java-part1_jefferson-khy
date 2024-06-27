a7b06ee9a7b82c171cbdd7ab2e9e932494541e5c
Jefferson Khy <jeffersonk@tlgcohort.com>
2024-06-27 10:46:30 -0500

    initial commit

---
+++ Lab01.1/src/HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+ * Classic Hello World application in Java.
+ * This is a standalone application, i.e., it does not run on an app server.
+ */
+class HelloWorld {
+
+    // starting point or entry point for every standalone Java application
+    public static void main(String[] args) {
+        System.out.println("Hello from IntelliJ");
+        System.out.println("This is nicer than bare metal programming");
+    }
+}
