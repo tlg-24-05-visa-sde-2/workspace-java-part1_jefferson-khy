23eb87a9639b7d03266c659f0c7817caab3e0086
Jefferson Khy <jeffersonk@tlgcohort.com>
2024-07-05 12:05:42 -0500

    smart enum genre with display property

--- Scratch/src/Genre.java
+++ Scratch/src/Genre.java
@@ -1,3 +1,27 @@
 enum Genre {
-    ANIME, DRAMA, HORROR, ROMANTIC_COMEDY, DOCUMENTARY, MYSTERY, SCI_FI, INTERNATIONAL
+    ANIME("Anime"),            //each of these calling the Genre() ctor below
+    DRAMA("Drama"),
+    HORROR("HORROR!!!"),
+    ROMANTIC_COMEDY("Romantic Comedy"),
+    DOCUMENTARY("Documentary"),
+    MYSTERY("Mystery"),
+    SCI_FI("Sci-Fi"),
+    INTERNATIONAL("International");
+
+    //everything under here is regular class definition stuff. fields, ctors, methods.
+    private final String display;
+
+    //implicitly private, no new from outside
+    Genre(String display){
+        System.out.println("Genre ctor called");
+        this.display = display;
+    }
+
+    public String getDisplay() {
+        return display;
+    }
+
+    public String toString() {
+        return getDisplay();
+    }
 }
